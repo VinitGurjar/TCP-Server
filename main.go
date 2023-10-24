8a37b99f696cf1be4f445bfb06181e268735950c
vinit gurjar <vinitfagna@gmail.com>
2023-10-24 18:40:26 +0000

    ⚒️chore: Read the request and reponse with a note

--- main.go
+++ main.go
@@ -2,10 +2,24 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
+	"time"
 )
+
+func do(conn net.Conn) {
+	buf := make([]byte, 1024)
+	//skiping the no of byte by writing '_'
+	_, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Memeking some fake process
+	time.Sleep(1 * time.Second)
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, Golang!\r\n"))
+	//Close connection
+	conn.Close()
+}
 
 func main() {
 	//Reserving a port for our tcp server to listen to client request.
@@ -20,5 +34,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(conn)
+	do(conn)
+
 }
